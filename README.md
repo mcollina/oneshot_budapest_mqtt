4d8fb5a5bc3ddd656c7d01db971e438c9565f2f9
Matteo Collina <hello@matteocollina.com>
2014-10-17 17:04:05 +0200

    Initial import.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-skillsmatter_fullstack_mqtt
-===========================
+Live code the Internet of Things
+===============================
+
+> A [Bespoke.js](http://markdalgleish.com/projects/bespoke.js) presentation, built with [generator-bespoke](https://github.com/markdalgleish/generator-bespoke)
+
+Abstract
+--------
+
+What can you build in 30 minutes, on stage, with a Tessel, a couple of Arduinos, some sensors and actuators? Is the IoT within reach? Can you do that yourself in the hackerspace this afternoon? In this talk I will demo all the relevant technologies for building an IoT prototype: the MQTT protocol, the Mosca broker, and how to use it from real hardware. Bonus points: I will also show how to connect a BLE device from node!
+
+View slides locally
+-------------------
+
+First, ensure you have the following installed:
+
+1. [Node.js](http://nodejs.org)
+2. [Bower](http://bower.io): `$ npm install -g bower`
+3. [Gulp](http://gulpjs.com): `$ npm install -g gulp`
+
+Then, install dependencies and run the preview server:
+
+```bash
+$ npm install && bower install
+$ gulp serve
+```
---
+++ slider.js
@@ -0,0 +1,62 @@
+#! /usr/bin/env node
+
+if (!process.env.DEBUG)
+  process.env.DEBUG = 'blue'
+
+var debug = require('debug')('blue')
+  , SensorTag = require('sensortag')
+  , press = require('mac-key-press').press
+  , leftKey = 123
+  , rightKey = 124
+  , client = require('mqtt').createClient()
+
+debug('searching for a sensor tag')
+
+SensorTag.discover(function(sensorTag) {
+  debug('discovered', sensorTag.uuid)
+
+  sensorTag.connect(function() {
+    debug('connected')
+    sensorTag.discoverServicesAndCharacteristics(function() {
+      debug('discovered all characteristics')
+      sensorTag.notifySimpleKey(function() {
+        debug('key press correctly setted up')
+      })
+
+      sensorTag.enableHumidity(function() {
+        sensorTag.notifyHumidity(function() {
+          debug('humidity correctly setted up')
+        })
+      })
+
+      sensorTag.enableIrTemperature(function() {
+        sensorTag.notifyIrTemperature(function() {
+          debug('ir temperature correctly setted up')
+        })
+      })
+    })
+  })
+
+  sensorTag.on('simpleKeyChange', function(left, right) {
+    if (left) {
+      debug('left pressed')
+      //press(leftKey)
+      client.publish('deck/prev');
+    } else if (right) {
+      debug('right pressed')
+      client.publish('deck/next');
+      //press(rightKey)
+      press(36) // return
+    }
+  })
+
+  sensorTag.on('humidityChange', function(temperature, humidity) {
+    client.publish('sensortag/temperature', '' + temperature)
+    client.publish('sensortag/humidity', '' + humidity)
+  })
+
+  sensorTag.on('irTemperatureChange', function(objectTemperature, ambientTemperature) {
+    client.publish('sensortag/ir/object', '' + objectTemperature)
+    client.publish('sensortag/ir/ambient', '' + ambientTemperature)
+  })
+})
